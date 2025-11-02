a889f79b1750eb6c785ef9ab5d0a82e90608fa26
jinwoow <0812jinwoo@gmail.com>
2025-11-03 01:51:21 +0900

    두번쨰 수정

--- src/main/kotlin/GIts.kt
+++ src/main/kotlin/GIts.kt
@@ -3,7 +3,8 @@ package com.example
 fun main() {
     println("hello")
 
-    var myName: String? = null
+//    var myName: String? = null
+    var myName: String? = "이이호"
 
     if (myName != null) {
         println("myName: ${myName} - 데이터 없음")
@@ -11,4 +12,13 @@ fun main() {
     else {
         println("MyName: ${myName}")
     }
+
+//    var nullname: String = if (myName == null) "데이터 없음" else myName
+    var nullname: String = myName ?: "이름없음"
+    println(nullname)
+
+    val othername : String = myName.let {
+        it + " - 데이터 존재"
+    }?: "이름없음"
+    println(othername)
 }
