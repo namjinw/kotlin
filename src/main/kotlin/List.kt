49e1adcd761e55495916f96362d76d90d10f1e64
jinwoow <0812jinwoo@gmail.com>
2025-11-03 02:10:16 +0900

    리스트 완성

--- src/main/kotlin/List.kt
+++ src/main/kotlin/List.kt
@@ -1,5 +1,21 @@
 package com.example
 
 fun main() {
-    var friends = listOf<Any>("철수", 1, "영의", " 제임스")
+//    var friends = listOf<Any>("철수", 1, "영의", " 제임스")
+//    var friends = listOf<String>("철수", "영의", " 제임스")
+    var friends = mutableListOf<String>("철수", "영의", "제임스")
+
+//    val friend = friends.get(0)
+//    val friend = friends[0]
+//    val friend = friends.first()
+    val friend = friends.last()
+
+    println(friend)
+
+    friends[0] = "이이호"
+    println(friends)
+
+    val mynumber = arrayOf(1,2,3,4)
+    mynumber[2] = 6
+    println(mynumber.contentToString())
 }
