8aecab4d19cd911a51a903de007e61a4ad8c12a6
jinwoow <0812jinwoo@gmail.com>
2025-11-03 01:27:01 +0900

    두번쨰 수정

--- src/main/kotlin/GIts.kt
+++ src/main/kotlin/GIts.kt
@@ -2,4 +2,13 @@ package com.example
 
 fun main() {
     println("hello")
+
+    var myName: String? = null
+
+    if (myName != null) {
+        println("myName: ${myName} - 데이터 없음")
+    }
+    else {
+        println("MyName: ${myName}")
+    }
 }
